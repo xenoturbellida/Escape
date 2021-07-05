3131462e0db96c4026a819bc776ffa4bb726f800
xenoturbellida <ElPGordeeva@stud.kpfu.ru>
2021-07-05 17:33:53 +0300

    Fix camera

--- Escape From The Professor/Assets/ThirdPersonMovement.cs
+++ Escape From The Professor/Assets/ThirdPersonMovement.cs
@@ -22,7 +22,7 @@ public class ThirdPersonMovement : MonoBehaviour
         if (direction.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            float angle = Mathf.SmoothDampAngle(transform.rotation.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
