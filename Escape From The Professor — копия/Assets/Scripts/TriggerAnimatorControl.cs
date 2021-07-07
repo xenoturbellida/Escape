95b11baac92c93e5eb36da079a87dd454db135a4
Anna <annasureeva3955@gmail.com>
2021-07-07 17:07:53 +0300

    prefab

---
+++ Escape From The Professor — копия/Assets/Scripts/TriggerAnimatorControl.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerAnimatorControl : MonoBehaviour {
+	public string nameTrigger;
+	public GameObject gameObjects;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.tag == "Player") {
+			gameObjects.GetComponent<Animator> ().SetTrigger (nameTrigger);
+		}
+	}
+}
