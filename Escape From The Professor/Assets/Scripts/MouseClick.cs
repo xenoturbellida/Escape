963afc65d3ed01c1662d274cfb0c2dd2a6f10d09
Anna <annasureeva3955@gmail.com>
2021-07-07 16:57:53 +0300

    pipe puzzle

---
+++ Escape From The Professor/Assets/Scripts/MouseClick.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseClick : MonoBehaviour
+{
+
+    // [SerializeField] private float[] Angle;
+	
+	public float properAngle;
+	   
+	public float smooth = 95.0f;
+
+    [SerializeField]
+    private float rotationStep = 90.0f;
+    
+    public bool position;
+
+    private Quaternion targetRot;
+
+    void Start()
+    {
+	    targetRot = transform.rotation;
+    }
+
+
+    void OnMouseDown()
+    {
+
+        transform.rotation = Quaternion.Euler(0,0, rotationStep) * transform.rotation;
+
+    }
+}
