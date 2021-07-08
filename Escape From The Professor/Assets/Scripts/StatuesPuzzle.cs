870fbb0e13078b0d5e3167bfadaa217e45d107e9
xenoturbellida <ElPGordeeva@stud.kpfu.ru>
2021-07-08 11:28:57 +0300

    Create prefabs (puzzle with statues, lifting door)

--- Escape From The Professor/Assets/Scripts/StatuesPuzzle.cs
+++ Escape From The Professor/Assets/Scripts/StatuesPuzzle.cs
@@ -7,13 +7,41 @@ public class StatuesPuzzle : MonoBehaviour
     public StatueRotation leftScript;
     public StatueRotation centerScript;
     public StatueRotation rightScript;
-    
+
+    public GameObject door;
+    public Transform startMarker;
+    public Transform endMarker;
+    public float speed = 0.001f;
+    public float startTime;
+    public float journeyLength;
+
+    public float distCovered;
+
+    void Start()
+    {
+        startTime = Time.time;
+        journeyLength = Vector3.Distance(startMarker.position, endMarker.position);
+    }
     // Update is called once per frame
     void Update()
     {
+        // LiftDoor();
         if (leftScript.properPos && centerScript.properPos && rightScript.properPos)
         {
-            Debug.Log("The puzzle with statues was solved.");
+            Debug.Log("i am opened");
+            LiftDoor();
+        }
+    }
+
+    void LiftDoor()
+    {
+        distCovered = (Time.time - startTime) * speed;
+        if (distCovered < journeyLength)
+        {
+            Debug.Log(door.transform.position.y);
+            Debug.Log(endMarker.position.y);
+            float fractionOfJourney = distCovered / journeyLength;
+            door.transform.position = Vector3.Lerp(startMarker.position, endMarker.position, fractionOfJourney);
         }
     }
 }
